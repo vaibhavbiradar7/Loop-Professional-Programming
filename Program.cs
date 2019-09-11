a843550dd2b89b3e27045d0a86fd5be75565a743
Atin Maiti <atinmaiti@mail.usf.edu>
2019-09-10 21:00:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loop_Professional_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many years of experience do you have in professional programming?");
+
+            try
+            {
+                int years = int.Parse(Console.ReadLine());
+                for (int i = 0; i < years; i++)
+                {
+                    Console.WriteLine("You have " + i + " years of exprience");
+                }
+                Console.WriteLine("Press any key to exit the program ...");
+
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program ...");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}    
